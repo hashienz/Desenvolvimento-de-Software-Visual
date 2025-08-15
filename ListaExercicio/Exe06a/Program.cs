b6d7f03b7eff66e190fa69b46676f5e3f06ec67f
ENZO RICARDO HASHIMOTO <enzo.hashimoto@cs.up.edu.br>
2025-08-15 11:17:53 -0300

    Ambientação e lista de Exercicios

---
+++ ListaExercicio/Exe06a/Program.cs
@@ -0,0 +1,41 @@
+﻿int tamanho = 100;
+int[] vetor = new int[tamanho];
+
+Random random = new Random();
+for (int i = 0; i < vetor.Length; i++)
+{
+    vetor[i] = random.Next(1000);
+}
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
+
+bool trocou;
+
+do
+{
+    trocou = false;
+    for (int i = 0; i < vetor.Length - 1; i++)
+    {
+    
+        if (vetor[i] > vetor [i + 1])
+        {
+            int auxiliar = vetor[i];
+            vetor[i] = vetor[i + 1];
+            vetor[i + 1] = auxiliar;
+            trocou = true;
+        }
+    }
+} while (trocou);
+
+
+Console.WriteLine("\n");
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
+
+
+
---
+++ ListaExercicio/Exe06b/Program.cs
@@ -0,0 +1,27 @@
+﻿int tamanho = 100;
+int[] vetor = new int[tamanho];
+
+Random random = new Random();
+for (int i = 0; i < vetor.Length; i++)
+{
+    vetor[i] = random.Next(1000);
+}
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
+
+// Ordenar com função 
+Array.Sort(vetor);
+
+foreach (int n in vetor)
+{
+    Console.WriteLine(n);
+}
+
+
+
+
+
+
