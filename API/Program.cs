dfe8ae31d2dd7a7f9f281e2ad0a5f40012d309e9
ENZO RICARDO HASHIMOTO <enzo.hashimoto@cs.up.edu.br>
2025-09-05 11:27:20 -0300

    Aula 05.09

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,36 @@
+using API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Listas com produtos
+List<Produto> listaDeProdutos = new List<Produto>
+{
+    new Produto { Nome = "Teclado Mecânico", Quantidade = 10, Preco = 199.99 },
+    new Produto { Nome = "Mouse Gamer", Quantidade = 15, Preco = 149.90 },
+    new Produto { Nome = "Monitor", Quantidade = 5, Preco = 899.00 },
+    new Produto { Nome = "Cadeira Gamer", Quantidade = 7, Preco = 1200.50 },
+    new Produto { Nome = "Headset", Quantidade = 20, Preco = 299.90 }
+};
+
+app.MapGet("/", () => "API de Produtos");
+
+app.MapGet("/api/produto/listar", () =>
+{
+    return listaDeProdutos;
+
+});
+
+app.MapPost("/api/produto/cadastrar", (Produto produto) =>
+{
+    
+    listaDeProdutos.Add(produto);
+
+});
+
+
 
 app.Run();
+
+
+
